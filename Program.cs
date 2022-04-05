6569d499d46b363c20c619e0264015b496286a74
Alen Veljan <Alen.Veljan071@gmail.com>
2022-04-05 15:02:03 +0200

    Feature/recursion

--- Program.cs
+++ Program.cs
@@ -1,58 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataStructureRecursion
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Node root = new Node("A");
-            Node n1 = new Node("B");
-            Node n2 = new Node("C");
-            Node n3 = new Node("D");
-            Node n4 = new Node("E");
-            Node n5 = new Node("F");
-            Node n6 = new Node("G");
-            Node n7 = new Node("I");
-            Node n8 = new Node("J");
 
+            var listChar = new List<Child>();
+            listChar.Add(new Child() { Id = 1, Name = 'A', ParentId = null });
+            listChar.Add(new Child() { Id = 2, Name = 'B', ParentId = 1 });
+            listChar.Add(new Child() { Id = 3, Name = 'C', ParentId = 1 });
+            listChar.Add(new Child() { Id = 4, Name = 'D', ParentId = 2 });
+            listChar.Add(new Child() { Id = 5, Name = 'E', ParentId = 3 });
+            listChar.Add(new Child() { Id = 6, Name = 'F', ParentId = 3 });
+            listChar.Add(new Child() { Id = 7, Name = 'G', ParentId = 2 });
+            listChar.Add(new Child() { Id = 8, Name = 'I', ParentId = 4 });
+            listChar.Add(new Child() { Id = 9, Name = 'J', ParentId = 8 });
 
-            root.left = n1;
-            root.right = n2;
-            n1.left = n3;
-            n1.right = n6;
-            n2.left = n4;
-            n2.right = n5;
-            n3.left = n7;
-            n7.right = n8;
-
-            BinarySearchTree.PreOrder(root);
+            Task3 tree = new Task3();
+            Node node = tree.createTree(listChar);
+            tree.preorder(node);
         }
 
+        class Task3
+        {
+            public Node root;
+            List<Node> nodes = new List<Node>();
+            public virtual void createNode(Node created)
+            {
+                nodes.Add(created);
+              
+                if (created.parent == null)
+                {
+                    root = created;
+                    return;
+                }
+
+                if (created.parent != null)
+                {
+                    Node p = nodes.Where(x => x.key == created.parent).FirstOrDefault();
+                   
+                    if (p.left == null)
+                    {
+                        p.left = created;
+                    }
+                    else 
+                    {
+                        p.right = created;
+                    }
+                }
+             
+            }
+            public virtual Node createTree( List<Child> lista)
+            {
+                foreach (var item in lista)
+                {
+                    var node = new Node(item.Id, item.Name, item.ParentId);
+                    createNode(node);
+                }
+                return root;
+            }
+            public virtual void preorder(Node node)
+            {
+                if (node != null)
+                {
+                    Console.WriteLine(node.name + " ");
+                    preorder(node.left);
+                    preorder(node.right);
+                }
+            }
+        }
         class Node
         {
-            public string value;
-            public Node left;
-            public Node right;
-            public Node(string value)
+            public char name;
+            public int? parent;
+            public int key;
+            public Node left, right;
+
+            public Node(int key, char name, int? parent)
             {
-                this.value = value;
+                this.name = name;
+                this.key = key;
+                left = right = null;
+                this.parent = parent;
             }
         }
-        class BinarySearchTree
+        class Child
         {
-            public static void PreOrder(Node root)
-            {
-                Console.WriteLine(root.value);
-                if (root.left != null)
-                {
-                    PreOrder(root.left);
-                }
-                if (root.right != null)
-                {
-                    PreOrder(root.right);
-                }
-            }
+            public int Id { get; set; }
+            public char Name { get; set; }
+            public int? ParentId { get; set; }
         }
     }
 }
+
